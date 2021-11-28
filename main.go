74e070251d4c8e2f8f69dc589ccf812168275139
Jesse Molina <jesse@jessemolina.xyz>
2021-11-28 11:58:37 -0600

    init POST and GET recipes

--- main.go
+++ main.go
@@ -1,10 +1,51 @@
 package main
 
 import (
+	"net/http"
+	"time"
+
 	"github.com/gin-gonic/gin"
+	"github.com/rs/xid"
 )
+
+// array of recipes; temporary memory storage
+var recipes []Recipe
+
+// json struct for recipe
+type Recipe struct {
+	ID   string `json:"id"`
+	Name string `json:"name"`
+	Tags []string `json:"tags"`
+	Ingredients []string `json:"ingredients"`
+	Instructions []string `json:"instructions"`
+	PublishedAt time.Time `json:"publishedAt"`
+}
+
+func NewRecipeHandler(c *gin.Context) {
+	var recipe Recipe
+	if err := c.ShouldBindJSON(&recipe); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": err.Error()})
+		return
+	}
+	recipe.ID = xid.New().String()
+	recipe.PublishedAt = time.Now()
+	recipes = append(recipes, recipe)
+	c.JSON(http.StatusOK, recipe)
+}
+
+func ListRecipesHandler(c *gin.Context) {
+	c.JSON(http.StatusOK, recipes)
+}
+
+func init() {
+	// iniate recipe array
+	recipes = make([]Recipe, 0)
+}
 
 func main() {
 	router := gin.Default()
+	router.POST("/recipes", NewRecipeHandler)
+	router.GET("/recipes", ListRecipesHandler)
 	router.Run()
 }
